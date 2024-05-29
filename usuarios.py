d3d3bbfc36ed31bc900a093ac786372eafeaf606
nini <sca.perezmQduocuc.cl>
2024-05-29 10:36:33 -0400

    inicio menu

---
+++ usuarios.py
@@ -0,0 +1,11 @@
+import os
+os.stem("cls")
+
+while True:
+    print("""
+    1. Inicio sesión
+    2. Registrar usuario
+    3. Eliminar usuario
+    4. Salir
+    """)
+    opc = int(input("Ingrese opción: "))
