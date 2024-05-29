cf7c8960b67c50ef1d2bd3eb3d2ccfc2b79db19e
nini <sca.perezmQduocuc.cl>
2024-05-29 10:59:53 -0400

    registro de usuario

--- usuarios.py
+++ usuarios.py
@@ -1,6 +1,8 @@
 import os
 os.stem("cls")
 
+usuarios = []
+
 while True:
     print("""
     1. Inicio sesión
@@ -9,3 +11,34 @@ while True:
     4. Salir
     """)
     opc = int(input("Ingrese opción: "))
+
+    if opc == 1:
+        pass
+    elif opc == 2:
+        print("REGISTRAR USUARIO")
+        while True:
+           user = input("Ingrese nombre de usuario: ")
+           if len(user) > 3 and user.isalpha:
+               print("usuario registrado!")
+               break
+           else:
+               print("ERROR! vuelva a ingresar nombre de usuario")
+
+        while True:
+            contra = input("Ingrese contraseña: ")
+            if contra in usuarios:
+                print("contraseña ya existente!")
+                continue
+            else:
+                break
+        
+        usuarios ={
+            "user": user,
+            "contra": contra
+        }
+
+
+    elif opc == 3:
+        pass
+    else:
+        break
